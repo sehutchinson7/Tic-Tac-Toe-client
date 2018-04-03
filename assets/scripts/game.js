b66ed297855040335c1cf0896f67f3979336fa3e
sehutchinson7 <sehutchinson7@gmail.com>
2018-04-02 20:04:46 -0400

    Commit Board Fixes
    Clicking on the cells in the game board now displays
    X's and O's instead of cell ID value.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -5,7 +5,7 @@ const gameArray = ['', '', '', '', '', '', '', '', '']
 // Define Player 1 as X and Player 2 as O
 const playerOne = 'X'
 const playerTwo = 'O'
-
+let currentPlayer = 'X' // Define currentPlayer to start as X
 // Makes the table clickable
 const addHandlers = function () {
   $('.cell').on('click', function (event) {
@@ -13,11 +13,18 @@ const addHandlers = function () {
     console.log('You clicked me!', event.target) // Log the event.target
     const id = this.id
     console.log(id)
-    $('#' + id).text(this.id) // Displays ID of array on the game board
+    // $('#' + id).text(this.id)
+    // Displays ID of array on the game board
+    // Add currentPlayer token to event.Target
+    $(event.target).text(currentPlayer)
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O'
+    } else {
+      currentPlayer = 'X'
+    }
   })
 }
 
-// Take turns between two players
 module.exports = {
   addHandlers,
   gameArray,
