bd86b93cba8bd66cdc6432b5e5d79bf9a2e231a0
sehutchinson7 <sehutchinson7@gmail.com>
2018-04-04 15:40:33 -0400

    Commit Display messages
    Created a message that displays on screen when a player wins.
    Moved conditions for take turns into a separate function takeTurns.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -34,44 +34,73 @@ const addHandlers = function () {
       const id = event.target.id // Add currentPlayer to JS board array
       gameArray[id] = currentPlayer
       $(event.target).text(currentPlayer) // Adc currentPlayer to HTMLL board
-      findWinner() // log if a person won
-      if (currentPlayer === 'X') { // change player
-        currentPlayer = 'O'
-        onPlayerMove()
-      } else {
-        currentPlayer = 'X'
-        onPlayerMove()
+      if (findWinner() === false) {
+        takeTurns()
       }
     } else {
-      console.log('This spot is taken!')
+      onTaken()
     }
   })
 }
 
+const takeTurns = function () {
+  if (currentPlayer === 'X') { // change player
+    currentPlayer = 'O'
+    onPlayerMove()
+  } else {
+    currentPlayer = 'X'
+    onPlayerMove()
+  }
+}
+
+const onWinner = function () {
+  $('.playerMove').html('Player ' + currentPlayer + ' wins!')
+}
+
 const onPlayerMove = function () {
-  console.log('Player ' + currentPlayer + "'s turn")
   $('.playerMove').html('Player ' + currentPlayer + "'s turn")
+}
+
+const onTaken = function () {
+  $('.playerMove').html('This spot is taken! Choose an empty box.')
 }
 
 const findWinner = function () {
   if (gameArray[0] === gameArray[1] && gameArray[1] === gameArray[2] && gameArray[0] !== '' && gameArray[1] !== '' && gameArray[2] !== '') {
-    console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo top row') // [0, 1, 2]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo top row') // [0, 1, 2]
   } else if (gameArray[0] === gameArray[4] && gameArray[4] === gameArray[8] && gameArray[0] !== '' && gameArray[4] !== '' && gameArray[8] !== '') {
-    console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo diagonal') // [0, 4, 8]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo diagonal') // [0, 4, 8]
   } else if (gameArray[2] === gameArray[5] && gameArray[5] === gameArray[8] && gameArray[2] !== '' && gameArray[5] !== '' && gameArray[8] !== '') {
-    console.log('Player ' + gameArray[2] + ' Wins!' + ' Winning combo diagonal') // [2, 5, 8]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[2] + ' Wins!' + ' Winning combo diagonal') // [2, 5, 8]
   } else if (gameArray[0] === gameArray[3] && gameArray[3] === gameArray[6] && gameArray[0] !== '' && gameArray[3] !== '' && gameArray[6] !== '') {
-    console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo far left column.') // [0, 3, 6]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[0] + ' Wins!' + ' Winning combo far left column.') // [0, 3, 6]
   } else if (gameArray[6] === gameArray[7] && gameArray[7] === gameArray[8] && gameArray[6] !== '' && gameArray[7] !== '' && gameArray[8] !== '') {
-    console.log('Player ' + gameArray[6] + ' Wins!' + ' Winning combo last row.') // [6, 7, 8]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[6] + ' Wins!' + ' Winning combo last row.') // [6, 7, 8]
   } else if (gameArray[3] === gameArray[4] && gameArray[4] === gameArray[5] && gameArray[3] !== '' && gameArray[4] !== '' && gameArray[5] !== '') {
-    console.log('Player ' + gameArray[3] + ' Wins!' + ' Winning combo middle row.') // [3, 4, 5]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[3] + ' Wins!' + ' Winning combo middle row.') // [3, 4, 5]
   } else if (gameArray[1] === gameArray[4] && gameArray[4] === gameArray[7] && gameArray[1] !== '' && gameArray[4] !== '' && gameArray[7] !== '') {
-    console.log('Player ' + gameArray[1] + ' Wins!' + ' Winning combo middle column') // [1, 4, 7]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[1] + ' Wins!' + ' Winning combo middle column') // [1, 4, 7]
   } else if (gameArray[2] === gameArray[4] && gameArray[4] === gameArray[6] && gameArray[2] !== '' && gameArray[4] !== '' && gameArray[6] !== '') {
-    console.log('Player ' + gameArray[2] + ' Wins!' + ' Winning combo diagonal 90 degree') // [2, 4, 6]
+    onWinner()
+    return true
+    //console.log('Player ' + gameArray[2] + ' Wins!' + ' Winning combo diagonal 90 degree') // [2, 4, 6]
   } else {
     console.log('Keep playing!')
+    return false
   }
 }
 
